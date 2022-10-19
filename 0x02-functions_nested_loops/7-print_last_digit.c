9f89fe802b27b99c6e2f239813b471fa14cd62ce
Blesaid <blesaidbassey@gmail.com>
2022-10-20 00:07:34 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - entey point
+ * @x: checked
+ * Return: x if true
+ */
+int print_last_digit(int x)
+{
+	int x;
+
+	y = n % 10;
+
+	_putchar ('y');
+	return (x);
+}
