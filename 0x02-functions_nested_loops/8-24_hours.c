fc1f618c646dbe90e84eab50acb1ffa1614be3d2
Blesaid <blesaidbassey@gmail.com>
2022-10-20 10:39:33 +0100

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point
+ * h: hour
+ * m: minute
+ * Returnv 24hours
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		_putchar((h / 10) +'0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+	}
+}
