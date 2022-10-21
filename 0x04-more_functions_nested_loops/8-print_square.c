4df956a2767918d6b572bbd4684f61267cb5f6e9
Blesaid <blesaidbassey@gmail.com>
2022-10-21 22:35:16 +0100

    tasks 8 & 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - entry point
+ * @size: checked
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; a++)
+		{
+			for (y = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
