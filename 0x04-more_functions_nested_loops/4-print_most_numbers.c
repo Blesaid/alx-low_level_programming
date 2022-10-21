c4173ee280284f2f48362b486c7b47756389af83
Blesaid <blesaidbassey@gmail.com>
2022-10-21 15:27:28 +0100

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,9 @@ void print_most_numbers(void)
 	for (x >= 0; x <= 9; x++)
 	{
 		if ((x == 2) || (x == 4))
-		{
 			continue;
-		}
-		_putchar(x + '0');
+		else
+			_putchar(x + '0');
 	}
 	_putchar('\n');
 }
