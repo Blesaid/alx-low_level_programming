1946eb43e4e8e2ee6e0134ec6addcce163054355
Blesaid <blesaidbassey@gmail.com>
2022-10-21 20:47:48 +0100

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,14 @@ void more_numbers(void)
 {
 	int x, count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (x >= 0; x <= 14; x++)
 		{
 			if (x > 9)
+			{
 				_putchar((x / 10) + '0');
+			}
 			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
