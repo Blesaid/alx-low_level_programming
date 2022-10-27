04882d3a3e69738b6d26ac901f212bce32f03e77
Blesaid <blesaidbassey@gmail.com>
2022-10-28 00:00:25 +0100

    tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - entry point
+ * @a: integer
+ * @n: interger
+ */
+void reverse_array(int *a, int n)
+{
+	int x;
+	int y;
+
+	for (x = 0; x = n; x++;)
+	{
+		n++;
+		y = a[x];
+		a[x] = a[n];
+		a[n] = y;
+	}
+}
