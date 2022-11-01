de4ab0a248e4be9a4e5c5c4c9980fec22e67fe19
Blesaid <blesaidbassey@gmail.com>
2022-11-01 21:52:17 +0100

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - entry point
+ * @s: bytes
+ * @b: byte
+ * @n: buffer size
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+}
