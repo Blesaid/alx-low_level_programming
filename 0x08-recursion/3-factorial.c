f2dc0f0a194e4c800934519a159f797bc15e70b9
Blesaid <blesaidbassey@gmail.com>
2022-11-03 15:30:46 +0100

    tasks

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,13 +10,13 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	if (n >= 0 && n <= 1)
+	else if (n == 0)
 	{
 		return (1);
 	}
 	else if (n > 1)
 	{
-		return (n * !(n - 1));
+		return (n * factorial(n - 1));
 	}
 	else
 		return (n);
