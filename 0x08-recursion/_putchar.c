86ba7b076770b38b7349914cd32027299498e6b5
Blesaid <blesaidbassey@gmail.com>
2022-11-03 13:08:49 +0100

    tasks

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
- *_putchar - entry point
- *Return: 1
+ * _putchar - writes the character to stdout
+ * @c: character
+ * Return: 1 on success
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
