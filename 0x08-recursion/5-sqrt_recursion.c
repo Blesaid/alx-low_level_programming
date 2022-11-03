3e55a6b0c5c363af86fda1db9d6852d1f937a096
Blesaid <blesaidbassey@gmail.com>
2022-11-03 14:37:37 +0100

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,49 @@
 #include "main.h"
 
-int real_sqrt_recursion(int n, int i);
+int real_sqrt(int x, int i);
+int _sqrt_recursion(int n);
 
 /**
+ * real_sqrt - second entry point
+ * @x: integer
+ * @i: iterator
+ * Return: square root
+ */
+int real_sqrt(int x, int i)
+{
+	if (i >= x / 2)
+	{
+		return (-1);
+	}
+	if (i * i == x)
+	{
+		return (i);
+	}
+	return (real_sqrt(x, i + 1));
+}
+/**
  * _sqrt_recursion - entry point
+ * real_sqrt - second entry point
  * @n: integer
+ * @i: iterator
  * Return: integer
  */
 int _sqrt_recursion(int n)
 {
+	int x;
+	int i;
+
 	if (n < 0)
 	{
 		return (-1);
 	}
-	return (real_sqrt_recursion(n, 0));
-}
-
-/**
- * real_sqrt_recursion - second entry point
- * @n: integer
- * @i: iterator
- * Return: square root
- */
-int real_sqrt_recursion(int n, int i)
-{
-	if (i * i > n)
+	if (n == 1)
 	{
-		return (-1);
+		return (1);
 	}
-	if (i * i == 8)
+	if (n == 0)
 	{
-		return (i);
+		return (0);
 	}
-	return (real_sqrt_recursion(n, i + 1));
+	return (real_sqrt(x, i));
 }
-
