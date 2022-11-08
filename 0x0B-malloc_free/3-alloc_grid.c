0334b8fd796acb6b0b71a6a2682024347012a76d
Blesaid <blesaidbassey@gmail.com>
2022-11-08 10:28:11 +0100

    tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
  * alloc_grid - entry point
  * @width: input
  * @height: input
- * return: 0
+ * Return: 0
  */
 int **alloc_grid(int width, int height)
 {
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	a = malloc(sizeof(int) * height);
+	a = (int **)malloc(sizeof(int) * height);
 
 	if (a == NULL)
 	{
@@ -23,16 +23,21 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(sizeof(int) * width);
+		a[i] = (int *)malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
 			free(a);
+
+			for (n = 0; n <= i; n++)
+				free(a[n]);
 			return (NULL);
 		}
-
+	}
+	for (i = 0; i < height; i++)
+	{
 		for (n = 0; n < width; n++)
 		{
-			free(a[n]);
+			a[i][n] = 0;
 		}
 	}
 	return (a);
